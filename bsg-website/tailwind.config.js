3753739d1923b6af0985130273e2c4abb338933c
Emma <88465944+emijane@users.noreply.github.com>
2023-11-15 14:01:52 -0500

    hero and nav bar updates

--- bsg-website/tailwind.config.js
+++ bsg-website/tailwind.config.js
@@ -9,6 +9,31 @@ export default {
       backgroundImage: {
         'hero': "url('/src/images/hero.png')",
       },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1.1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
     },
   },
   plugins: [],
