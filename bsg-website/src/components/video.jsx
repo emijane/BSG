b55a284efaf376e81ba0f6d216be8eb2eb559cbe
Emma <88465944+emijane@users.noreply.github.com>
2023-11-15 23:31:46 -0500

    added read.me

---
+++ bsg-website/src/components/video.jsx
@@ -0,0 +1,11 @@
+{/* Video Player component */}
+
+function Video () {
+    return (
+    <div>
+        <iframe width="1160" height="715" src="https://www.youtube.com/embed/-bz-qDxRRcg?si=NCf9HTZZhUb4ULCI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    )
+  }
+
+  export default Video;
