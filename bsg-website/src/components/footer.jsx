a3256d21c7fa93af61bd8729de404af2077224ee
Emma <88465944+emijane@users.noreply.github.com>
2023-11-21 23:05:35 -0500

    form validation

--- bsg-website/src/components/footer.jsx
+++ bsg-website/src/components/footer.jsx
@@ -14,13 +14,20 @@ function Footer () {
                 </ul>
                 </div>
                 <form 
-                className='flex flex-1 flex-col gap-4 items-center mt-5 w-full'
-                action="https://getform.io/f/728263a9-167f-416d-a856-dfdefdfc2403" method="POST">
-                    <input className='rounded-xl p-4 w-full' type="text" placeholder='Name' name="name" />
-                    <input className='rounded-xl p-4 w-full' type="email" placeholder='E-mail' name="email" />
-                    <input className='rounded-xl px-4 pb-12 w-full' type="text" placeholder='Message' name="message" />
-                    <input type="hidden" name="_gotcha" style={{ display: 'none' }} />
-                    <button type="submit" className='bg-orange-500 hover:bg-orange-600 hover:-translate-y-1 transition ease-in-out delay-50 duration-300 font-semibold text-white py-3 px-5 rounded-full w-full xl:max-w-[30%] xs:max-w-full'>Send Message</button>
+                    className='flex flex-1 flex-col gap-4 items-center mt-5 w-full'
+                    action="https://getform.io/f/728263a9-167f-416d-a856-dfdefdfc2403" 
+                    method="POST"
+                >
+                    <input className='rounded-xl p-4 w-full' type="text" placeholder='Name' name="name" required/>
+                    <input className='rounded-xl p-4 w-full' type="email" placeholder='E-mail' name="email" required />
+                    <input className='rounded-xl px-4 pb-12 w-full' type="text" placeholder='Message' name="message" required />
+                    <input type="hidden" name="_gotcha" style={{ display: 'none' }} required />
+                    <button 
+                        type="submit" 
+                        className='bg-orange-500 hover:bg-orange-600 hover:-translate-y-1 transition ease-in-out delay-50 duration-300 font-semibold text-white py-3 px-5 rounded-full w-full xl:max-w-[30%] xs:max-w-full'
+                    >
+                        Send Message
+                    </button>
                 </form>
             </div>
             <div className='p-10 text-center mt-10'>
